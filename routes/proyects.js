85f3cf8ce0c69a2a331f4f283746fb4af9bd22b9
JulianRB <ignis.lykaios@gmail.com>
2023-03-11 20:07:19 -0600

    Limpiados ls componentes de comentarios

--- routes/proyects.js
+++ routes/proyects.js
@@ -11,33 +11,13 @@ const {
   getMyProyects,
 } = require("../controllers/proyects");
 
-const urlValidator = function (value, helpers) {
-  if (validator.isURL(value)) {
-    return value;
-  }
-  return helpers.error("string.uri");
-};
-
 proyectsRoute.get("/", getProyects);
-proyectsRoute.post(
-  "/",
-  // celebrate({
-  //   [Segments.BODY]: Joi.object().keys({
-  //     img: Joi.string().custom(urlValidator),
-  //     name: Joi.string().required(),
-  //     city: Joi.string().required(),
-  //     description: Joi.string().required(),
-  //     discipline: Joi.string().required(),
-  //   }),
-  // }),
-  createProyect
-);
+proyectsRoute.post("/", createProyect);
 proyectsRoute.get("/created", getMyProyects);
 proyectsRoute.get("/:proyectId", getProyect);
 proyectsRoute.delete("/:proyectId", deleteProyect);
 proyectsRoute.patch("/:proyectId", editProyect);
 proyectsRoute.patch("/colaborate/:proyectId", proyectUpdateColaborators);
-// proyectsRoute.delete("/likes/:proyectId", unlikeproyect);
 proyectsRoute.use(errors());
 
 module.exports = { proyectsRoute };
